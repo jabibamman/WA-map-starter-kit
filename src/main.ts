0af5076af0bf1efce4fbabde1b8be93d4b0312d3
Charles <charlescretois@gmail.com>
2024-04-04 10:51:26 +0200

    feat/sleep-mode-init

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,70 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+import { Menu, Popup } from "@workadventure/iframe-api-typings";
+import { MenuOptions } from "@workadventure/iframe-api-typings/front/Api/Iframe/ui";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
-let currentPopup: any = undefined;
+let currentPopup: Popup | undefined = undefined;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    let sleepModeIsActive = false;
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    let currentSleepModeButton: Menu | undefined = undefined;
+
+    const changeSleepMode = () => {
+      console.log("Test boutton !");
+      sleepModeIsActive = !sleepModeIsActive;
+      if (currentSleepModeButton != undefined) {
+        currentSleepModeButton.remove();
+        currentSleepModeButton = WA.ui.registerMenuCommand(
+          sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
+          {
+            callback: () => {
+              changeSleepMode();
+            },
+          }
+        );
+      }
+    };
+
+    currentSleepModeButton = WA.ui.registerMenuCommand(
+      sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
+      {
+        callback: () => {
+          changeSleepMode();
+        },
+      }
+    );
+
+    WA.room.area.onEnter("clock").subscribe(() => {
+      const today = new Date();
+      const time = today.getHours() + ":" + today.getMinutes();
+      currentPopup = WA.ui.openPopup("clockPopup", "Ronan " + time, []);
+    });
+
+    WA.room.area.onLeave("clock").subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopup() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
 
 export {};
