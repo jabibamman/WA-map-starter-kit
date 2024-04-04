e75be9d3ced469a6cbceb5bdf295d232baf9c2ac
Alex091998 <alexrasiah@gmail.com>
2024-04-04 11:10:48 +0200

    Remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { Menu, Popup } from "@workadventure/iframe-api-typings";
-import { MenuOptions } from "@workadventure/iframe-api-typings/front/Api/Iframe/ui";
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {Menu, Popup} from "@workadventure/iframe-api-typings";
+import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 console.log("Script started successfully");
 
@@ -10,61 +9,61 @@ let currentPopup: Popup | undefined = undefined;
 
 // Waiting for the API to be ready
 WA.onInit()
-  .then(() => {
-    console.log("Scripting API ready");
-    console.log("Player tags: ", WA.player.tags);
+    .then(() => {
+        console.log("Scripting API ready");
+        console.log("Player tags: ", WA.player.tags);
 
-    let sleepModeIsActive = false;
+        let sleepModeIsActive = false;
 
-    let currentSleepModeButton: Menu | undefined = undefined;
+        let currentSleepModeButton: Menu | undefined = undefined;
 
-    const changeSleepMode = () => {
-      console.log("Test boutton !");
-      sleepModeIsActive = !sleepModeIsActive;
-      if (currentSleepModeButton != undefined) {
-        currentSleepModeButton.remove();
+        const changeSleepMode = () => {
+            console.log("Test boutton !");
+            sleepModeIsActive = !sleepModeIsActive;
+            if (currentSleepModeButton != undefined) {
+                currentSleepModeButton.remove();
+                currentSleepModeButton = WA.ui.registerMenuCommand(
+                    sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
+                    {
+                        callback: () => {
+                            changeSleepMode();
+                        },
+                    }
+                );
+            }
+        };
+
         currentSleepModeButton = WA.ui.registerMenuCommand(
-          sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
-          {
-            callback: () => {
-              changeSleepMode();
-            },
-          }
+            sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
+            {
+                callback: () => {
+                    changeSleepMode();
+                },
+            }
         );
-      }
-    };
 
-    currentSleepModeButton = WA.ui.registerMenuCommand(
-      sleepModeIsActive ? "Se Réveiller !" : "C'est l'heure de dormir ^^",
-      {
-        callback: () => {
-          changeSleepMode();
-        },
-      }
-    );
+        WA.room.area.onEnter("clock").subscribe(() => {
+            const today = new Date();
+            const time = today.getHours() + ":" + today.getMinutes();
+            currentPopup = WA.ui.openPopup("clockPopup", "Ronan " + time, []);
+        });
 
-    WA.room.area.onEnter("clock").subscribe(() => {
-      const today = new Date();
-      const time = today.getHours() + ":" + today.getMinutes();
-      currentPopup = WA.ui.openPopup("clockPopup", "Ronan " + time, []);
-    });
+        WA.room.area.onLeave("clock").subscribe(closePopup);
 
-    WA.room.area.onLeave("clock").subscribe(closePopup);
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra()
-      .then(() => {
-        console.log("Scripting API Extra ready");
-      })
-      .catch((e) => console.error(e));
-  })
-  .catch((e) => console.error(e));
+        // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+        bootstrapExtra()
+            .then(() => {
+                console.log("Scripting API Extra ready");
+            })
+            .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
 
 function closePopup() {
-  if (currentPopup !== undefined) {
-    currentPopup.close();
-    currentPopup = undefined;
-  }
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
 }
 
 export {};
